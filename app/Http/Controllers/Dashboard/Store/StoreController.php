2af07ff9ee6acf610004baeb78949f50d11c3bfa
Dimas Adi Satria <dimss.satria@gmail.com>
2018-02-27 10:50:25 +0700

    penambahan halaman store

--- app/Http/Controllers/Dashboard/Store/StoreController.php
+++ app/Http/Controllers/Dashboard/Store/StoreController.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\Controller;
 
 class StoreController extends Controller
 {
+	public function index()
+	{
+		return view('dashboard.store.store');
+	}
     public function user()
     {
     	return view('dashboard.store.store');
